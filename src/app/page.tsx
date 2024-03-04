dbcadc2dd438aed69e21b02a0cbb0d549f784060
kocharalampidis <kocharalampidis@gmail.com>
2024-03-04 17:47:58 +0200

    added template for cards and dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Signin from "./auth/signin/page";
+import CardDataStats from "./components/Cards";
 import DefaultLayout from "./components/Layouts/DefaultLayout";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <>
       <DefaultLayout>
-        <Signin />
+        {/* <Signin /> */}
+        {/* Refactor this brick  */}
+        <Dashboard />
       </DefaultLayout>
     </>
   );
